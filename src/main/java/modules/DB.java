1b71369f1be363666c9526e612d2d61bb5bc9ccf
saretoduncan <saretoduncan@gmail.com>
2021-08-16 12:55:12 +0300

    add database

---
+++ src/main/java/modules/DB.java
@@ -0,0 +1,9 @@
+package modules;
+import org.sql2o.Sql2o;
+
+public class DB{
+
+    public  static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker","softwaredev","k1pk0sg31");
+}
+
+
